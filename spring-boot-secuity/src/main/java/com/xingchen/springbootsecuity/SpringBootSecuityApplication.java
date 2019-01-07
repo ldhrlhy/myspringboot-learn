c9d438bd8f2b8dd826eedc03cbf5c62802f41702
dy <dy@dydeMacBook-Pro.lan>
2019-01-07 08:05:44 +0800

    spring secuity

---
+++ spring-boot-secuity/src/main/java/com/xingchen/springbootsecuity/SpringBootSecuityApplication.java
@@ -0,0 +1,14 @@
+package com.xingchen.springbootsecuity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecuityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecuityApplication.class, args);
+    }
+
+}
+
